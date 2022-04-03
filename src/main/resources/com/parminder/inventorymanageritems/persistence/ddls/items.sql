b8a92fcba757f9040ae214da1d6ea1ed21830f83
parminder <parmindertejpal@gmail.com>
2022-04-03 14:46:38 +0530

    Added intial data load

--- src/main/resources/com/parminder/inventorymanageritems/persistence/ddls/items.sql
+++ src/main/resources/com/parminder/inventorymanageritems/persistence/ddls/items.sql
@@ -5,3 +5,22 @@ ID number,NAME varchar2(100),DESCRIPTION varchar2(200),ACTIVE varchar2(1)
 CREATE TABLE monu.warehouses(
 ID number,NAME varchar2(100),DESCRIPTION varchar2(200),ACTIVE varchar2(1),acty_ts timestamp
 );
+/* Initial Population*/
+declare 
+begin
+for i in 1 .. 100
+loop
+insert into Items values ( i, 'Item ' || i, 'Test Item Desc ' || i, 'Y', systimestamp);
+end loop;
+commit;
+end;
+
+declare 
+begin
+for i in 1 .. 100
+loop
+insert into Warehouses values ( i, 'Warehouse ' || i, 'Test Warehouse Desc ' || i, 'Y', systimestamp);
+end loop;
+commit;
+end;
+
